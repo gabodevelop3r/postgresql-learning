0110be398bdf06156e1020ef246f4f5a9cf6c662
gabogallegosjyd <ggallegos@holdingjesydor.cl>
2026-06-07 00:38:13 -0400

    aggregate functions

---
+++ 02-aggregate-functions/excercises/aggregation.sql
@@ -0,0 +1,79 @@
+-- 1. Cuantos usuarios tenemos con cuentas @google.com
+-- Tip: count, like
+
+select count(*)                  as total_google_domain,
+       split_part(email, '@', 2) as domain
+from users
+where email like '%@google.com'
+group by split_part(email, '@', 2);
+
+
+-- 2. De qué países son los usuarios con cuentas de @google.com
+-- Tip: distinct
+select distinct (country)
+from users
+where email like '%@google.com'
+group by split_part(email, '@', 2), country;
+
+-- 3. Cuantos usuarios hay por país (country)
+-- Tip: Group by
+select count(*),
+       country
+from users
+group by country;
+
+-- 4. Listado de direcciones IP de todos los usuarios de Iceland
+-- Campos requeridos first_name, last_name, country, last_connection
+select first_name, last_name, country, last_connection
+from users
+where country ILIKE 'iceland';
+
+-- 5. Cuantos de esos usuarios (query anterior) tiene dirección IP
+-- que incia en 112.XXX.XXX.XXX
+select count(*)
+from users
+where country ILIKE 'iceland'
+  and last_connection like '112.%';
+
+-- 6. Listado de usuarios de Iceland, tienen dirección IP
+-- que inicia en 112 ó 28 ó 188
+-- Tip: Agrupar condiciones entre paréntesis 
+select first_name, last_name, last_connection
+from users
+where country ILIKE 'iceland'
+  and (
+    last_connection like '112.%' or last_connection like '28.%'
+        or last_connection like '188.%'
+    );
+-- 7. Ordene el resultado anterior, por apellido (last_name) ascendente
+-- y luego el first_name ascendentemente también
+select first_name, last_name, last_connection
+from users
+where country ILIKE 'iceland'
+  and (
+    last_connection like '112.%' or last_connection like '28.%'
+        or last_connection like '188.%'
+    )
+order by last_name, first_name asc;
+
+
+-- 8. Listado de personas cuyo país está en este listado
+-- ('Mexico', 'Honduras', 'Costa Rica')
+-- Ordenar los resultados de por País asc, Primer nombre asc, apellido asc
+-- Tip: Investigar IN
+-- Tip2: Ver Operadores de Comparación en la hoja de atajos (primera página)
+
+select *
+from users
+where country in ('Mexico', 'Honduras', 'Costa Rica')
+order by country, first_name, last_name asc
+
+-- 9. Del query anterior, cuente cuántas personas hay por país
+-- Ordene los resultados por País asc
+select count(country) as total_country,
+       country
+from users
+where country in ('Mexico', 'Honduras', 'Costa Rica')
+group by country
+order by country asc
+
--- 02-aggregate-functions/postgresql_aggregate_functions.sql
+++ 02-aggregate-functions/postgresql_aggregate_functions.sql
@@ -1,4 +1,5 @@
-select * from users;
+select *
+from users;
 
 select first_name,
        last_name,
@@ -12,42 +13,71 @@ select first_name,
        followers,
        following
 from users
-    where followers > 4600;
+where followers > 4600;
 
 
 select first_name,
        last_name,
        followers
 from users
-    where
-        -- followers > 4600 and followers < 4700
-        followers between 4600 and 4700
+where
+    -- followers > 4600 and followers < 4700
+    followers between 4600 and 4700
 order by followers desc;
 
 
-select count(*) as total_users,
-        min(followers) as min_followers,
-        max(followers) as max_followers,
-        round(avg(followers),0) as avg_followers,
-        sum(followers) / count(*) as avg_manual
+select count(*)                  as total_users,
+       min(followers)            as min_followers,
+       max(followers)            as max_followers,
+       round(avg(followers), 0)  as avg_followers,
+       sum(followers) / count(*) as avg_manual
 from users;
 
 select first_name,
        last_name,
        followers
-    from users
-    where followers = (
-        select min(followers)
-            from users
-        )
+from users
+where followers = (select min(followers)
+                   from users)
 group by first_name,
          last_name,
          followers;
 
 select count(*),
        followers
-    from users
-    where followers = 4 or followers = 4999
-group by followers ;
+from users
+where followers = 4
+   or followers = 4999
+group by followers;
 
+-- HAVING
+select country,
+       count(country) as total
+from users
+group by country
+having count(country) between 1 and 5
+order by count(country) asc
+;
 
+-- DISTINC
+select distinct (trim(lower(country))) as country
+from users
+group by country
+having count(country) > 0
+order by country asc
+;
+
+select count(*)                  as total,
+       split_part(email, '@', 2) as domain
+from users
+group by split_part(email, '@', 2)
+having count(split_part(email, '@', 2)) > 1
+;
+
+-- SUBQUERYS
+select sum(total)
+from (select count(*)                  as total,
+             split_part(email, '@', 2) as domain
+      from users
+      group by split_part(email, '@', 2)
+      having count(split_part(email, '@', 2)) > 1) as domains;
